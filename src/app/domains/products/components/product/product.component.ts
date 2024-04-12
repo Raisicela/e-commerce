d085d7afccbaf3164a584a7df601ffb7a6b807cb
Raisa Guayasamin <raisa.marilyn1993@gmail.com>
2024-04-12 12:02:07 -0500

    uso de @input y @output y estilos en list.components y product.compponents

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+  @Input({required:true}) img: string = '';
+  @Input({required:true}) price: number = 0;
+  @Input({ required: true }) title: string = '';
+
+  @Output() addToCart = new EventEmitter();
+
+  addToCartHandler() {
+    console.log('click form child')
+    this.addToCart.emit('hola este es un mensaje desde el hijo ' + this.title)
+
+  }
 
 }
