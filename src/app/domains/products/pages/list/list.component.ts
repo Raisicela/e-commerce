6591d220d3ed0b6c8de05439af62a0ebc732ae4c
Raisa Guayasamin <raisa.marilyn1993@gmail.com>
2024-04-15 12:03:41 -0500

    Creacion de header y estilos

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { Product } from '../../../shared/model/product.model';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
@@ -34,6 +35,41 @@ export class ListComponent {
         title: 'Pro 3',
         price: 100,
         image: 'https://picsum.photos/640/640?r=13',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=33',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 2',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=32',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 3',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=11',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 2',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=42',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 3',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=41',
         creationAt: new Date().toISOString(),
       },
     ];
